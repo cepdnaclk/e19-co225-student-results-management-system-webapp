29c8d9b3afcb425f9973a5f21698d949e43e9d15
Sanka Peeris <e19275@eng.pdn.ac.lk>
2023-06-04 11:34:44 +0530

    test simple functionality of spring-boot

---
+++ backend/src/main/java/com/acadametrics/Main.java
@@ -0,0 +1,19 @@
+package com.acadametrics;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+    @GetMapping("/")
+    @ResponseBody
+    public User greet(){
+        return new User("john", "123");
+    }
+}
---
+++ backend/src/main/java/com/acadametrics/User.java
@@ -0,0 +1,19 @@
+package com.acadametrics;
+
+public class User {
+    private String userName;
+    private String password;
+
+    User(String userName, String password){
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
