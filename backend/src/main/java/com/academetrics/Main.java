45f03de3af90d51aeba6fc64e45f6d31d4313252
Sanka Peeris <e19275@eng.pdn.ac.lk>
2023-06-04 14:15:57 +0530

    added demo http request handling

--- backend/src/main/java/com/academetrics/Main.java
+++ backend/src/main/java/com/academetrics/Main.java
@@ -1,8 +1,7 @@
-package com.acadametrics;
+package com.academetrics;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -11,9 +10,5 @@ public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
-    @GetMapping("/")
-    @ResponseBody
-    public User greet(){
-        return new User("john", "123");
-    }
+
 }
