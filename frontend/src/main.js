2498639de8eadbd2ac7ee9499f997730219b8020
inesha21 <e19090@eng.pdn.ac.lk>
2023-06-17 16:32:10 +0530

    fontawesome

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,4 +6,16 @@ import store from "./store";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-createApp(App).use(store).use(router).mount("#app");
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faUserSecret, faUserGraduate } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faUserSecret, faUserGraduate)
+
+createApp(App).use(store).use(router).component('font-awesome-icon', FontAwesomeIcon).mount("#app");
