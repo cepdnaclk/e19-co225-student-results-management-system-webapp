a6e7c9725ee32cd831819b1605323805390a66a4
KalinduWijerathna <e19446@eng.pdn.ac.lk>
2023-06-18 09:32:26 +0530

    add sample data

--- backend/academetricsDBupdated.sql
+++ backend/academetricsDBupdated.sql
@@ -149,3 +149,62 @@ CREATE TABLE IF NOT EXISTS `user` (
   FOREIGN KEY (`Dep_id`) REFERENCES `department`(`D_id`) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
+-- -----------------------------------------------------------------------
+-- Inserting Sample Data Recodrs
+-- Insert data into the department table
+INSERT INTO department (D_id, D_name)
+VALUES
+    (1, 'Civil Engineering'),
+    (2, 'Chemical and Processing Engineering'),
+    (3, 'Computer Engineering'),
+    (4, 'Electrical and Electronic Engineering'),
+    (5, 'Manufacturing and Industrial Engineering'),
+    (6, 'Mechanical Engineering'),
+    (7, 'Engineering Management'),
+    (8, 'Engineering Mathematics');
+
+-- Insert data into the user table
+INSERT INTO user (Id, User_name, Password, Gmail, Initials, Last_name, sex, b_year, b_month, b_date, profile_picture, addressline1, addressline2, addressline3, Dep_id, contact_no)
+VALUES
+    (1, 'kavishkagaya', 'password', 'kavishkagaya@example.com', 'K.G.', 'Dissanayake', 1, 2000, 5, 10, null, '123 Main St', 'Colombo', 'Sri Lanka', 1, '1234567890'),
+    (2, 'gabrielinesha', 'password', 'ineshag@example.com', 'I.G.', 'De Silva', 1, 1999, 8, 15, null, '456 Elm St', 'Kandy', 'Sri Lanka', 2, '9876543210'),
+    (3, 'silvaboss', 'password', 'boss_silva@example.com', 'S.B', 'Rambukwella', 1, 1999, 3, 20, null, '789 Oak St', 'Galle', 'Sri Lanka', 3, '5678901234'),
+    (4, 'thiranjaya', 'password', 'thiranjaya@example.com', 'T.', 'Rajapaksa', 1, 1999, 11, 5, null, '321 Pine St', 'Jaffna', 'Sri Lanka', 4, '4321098765'),
+    (5, 'denuwankalindu', 'password', 'denuwankalindu@example.com', 'D.K.', 'Senevirathna', 1, 2000, 2, 1, null, '654 Maple St', 'Matara', 'Sri Lanka', 5, '9012345678'),
+    (6, 'manodya', 'password', 'gomezmanodya@example.com', 'M.', 'Gomez', 1, 2000, 9, 25, null, '987 Pine St', 'Negombo', 'Sri Lanka', 1, '7890123456'),
+    (7, 'dinukanemo', 'password', 'dinukanemo@example.com', 'D.N.', 'Wickramasinghe', 1, 2000, 4, 12, null, '654 Oak St', 'Colombo', 'Sri Lanka', 2, '5678901234'),
+    (8, 'sanka', 'password', 'peterson_sanka@example.com', 'S.M', 'Peterson', 1, 1999, 11, 8, null, '321 Elm St', 'Kandy', 'Sri Lanka', 3, '9012345678');
+
+-- Insert data into the student table
+INSERT INTO student (S_id, Batch, Semester, gpa, D_rank)
+VALUES
+    (1, 19, 2, 3.5, 1),
+    (2, 17, 2, 3.2, 2),
+    (3, 18, 2, 3.7, 3),
+    (4, 20, 2, 3.0, 4),
+    (5, 19, 2, 3.9, 5);
+
+-- Insert data into the course_offering table
+INSERT INTO course_offering (Course_id, Coord_id, offered_by, offered_to, Course_code, year, batch, semester, credits)
+VALUES
+    (1, 1, 3, 4, 'GP106', 2023, 20, 2, 3),
+    (2, 2, 3, 5, 'GP106', 2021, 19, 1, 3),
+    (3, 3, 3, 1, 'CO225', 2023, 19, 4, 3),
+    (4, 4, 8, 1, 'EM212', 2023, 19, 4, 2),
+    (5, 5, 3, 3, 'CO225', 2021, 18, 4, 3);
+
+-- Insert data into the assesment table
+INSERT INTO assesment (c_code, a_type, max_marks)
+VALUES
+    ('GP106', 'Mid Exam', 20),
+    ('GP106', 'Project', 10),
+    ('CO225', 'Project', 30),
+    ('CO225', 'Lab-1', 5),
+    ('CO225', 'Lab-2', 5),
+    ('CO225', 'Lab-3', 5),
+    ('CO225', 'Lab-4', 5),
+    ('CO225', 'Quiz-1', 5),
+    ('CO225', 'Quiz-2', 5),
+    ('EM215', 'Mid Exam', 20),
+    ('EM215', 'Lab Assignment -1', 5);
+
