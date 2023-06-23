aa47afca8c264630fa619ed604b6741c0c2bf4bd
inesha21 <e19090@eng.pdn.ac.lk>
2023-06-24 00:34:25 +0530

    bundled js file

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,6 +5,7 @@ import router from "./router";
 import store from "./store";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/js/bootstrap.bundle';
 import "bootstrap/dist/js/bootstrap.min.js";
 import axios from "axios";
 import "bootstrap-icons/font/bootstrap-icons.css";
