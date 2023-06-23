06a1de8bf344778df1a651d5fbd95fdcdc36a581
KalinduWijerathna <e19446@eng.pdn.ac.lk>
2023-06-23 16:33:36 +0530

    error correction in course/courseOffering

--- academetrics/src/main/java/com/academetrics/academetrics/Controller/CourseOfferingController.java
+++ academetrics/src/main/java/com/academetrics/academetrics/Controller/CourseOfferingController.java
@@ -1,13 +1,13 @@
 package com.academetrics.academetrics.Controller;
 
-import com.academetrics.academetrics.DTO.CourseDTO;
-import com.academetrics.academetrics.Entity.Course;
-import com.academetrics.academetrics.Service.CourseService;
+import com.academetrics.academetrics.DTO.CourseOfferingDTO;
+import com.academetrics.academetrics.Entity.CourseOffering;
+import com.academetrics.academetrics.Service.CourseOfferingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = "/course")
+@RequestMapping(value = "/courseOffering")
 @CrossOrigin
 public class CourseOfferingController {
 
@@ -24,7 +24,7 @@ public class CourseOfferingController {
 
     //  Show All Course Offerings
     @GetMapping("/") // Map only GET Requests
-    public @ResponseBody Iterable<Course> getAllCourseOfferings() {
+    public @ResponseBody Iterable<CourseOffering> getAllCourseOfferings() {
         //returns a JSON or XML with the courses
         return courseOfferingService.getAllCourseOfferings();
     }
@@ -38,9 +38,9 @@ public class CourseOfferingController {
     }
 
     @DeleteMapping("/")
-    public String deleteCourseOffering(@RequestParam String Code){
-
-        return CouurseOfferingService.deleteUser(Code);
+    public String deleteCourseOffering(@RequestParam String Code, int Year){
+        // check composite key for Course code and year
+        return CouurseOfferingService.deleteCourseOffering(Code);
     }
 
 }
