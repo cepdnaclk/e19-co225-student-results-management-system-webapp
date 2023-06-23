3b8a527680be12c3238cfb43f6171f29f2c1760e
inesha21 <e19090@eng.pdn.ac.lk>
2023-06-23 20:23:18 +0530

    Student paths

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -177,6 +177,44 @@ const routes = [
     name: "Student",
     component: () =>
       import(/* webpackChunkName: "Studenttemp" */ "../views/StudentMainView.vue"),
+    children: [
+      {
+        path: "home",
+        name: "StudentHome",
+        component: () =>
+        import(/* webpackChunkName: "StudentHome" */ "../views/StudentView/HomeView.vue"),  
+      },
+      {
+        path: "dashboard",
+        name: "StudentDashboard",
+        component: () =>
+        import(/* webpackChunkName: "StudentDashboard" */ "../views/StudentView/DashboardView.vue"),  
+      },
+      {
+        path: "targetdashboard",
+        name: "StudentTargetDashboard",
+        component: () =>
+        import(/* webpackChunkName: "StudentTargetDashboard" */ "../views/StudentView/TargetDashboardView.vue"),  
+      },
+      {
+        path: "courses",
+        name: "StudentCourses",
+        component: () =>
+        import(/* webpackChunkName: "StudentCourses" */ "../views/StudentView/CoursesView.vue"),  
+      },
+      {
+        path: "innercourses",
+        name: "StudentInnerCourses",
+        component: () =>
+        import(/* webpackChunkName: "StudentInnerCourses" */ "../views/StudentView/CoursesInnerView.vue"),  
+      },
+      {
+        path: "edit",
+        name: "StudentEdit",
+        component: () =>
+        import(/* webpackChunkName: "StudentEdit" */ "../views/StudentView/EditProfileView.vue"),  
+      }
+    ]
   },
 ];
 
