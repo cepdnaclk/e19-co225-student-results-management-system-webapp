99e0e9987c2db3c7e1f000e74e3a5a2c92d6559b
manodyaSenevirathne <e19366@eng.pdn.ac.lk>
2023-06-30 22:25:01 +0530

    Fixed Cookie Sending issue
    Added Cookie handling
    Added test login page
    Autherized all endpoints

--- academetrics/src/main/java/com/academetrics/academetrics/Configuration/SecurityConfig.java
+++ academetrics/src/main/java/com/academetrics/academetrics/Configuration/SecurityConfig.java
@@ -1,5 +1,9 @@
 package com.academetrics.academetrics.Configuration;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +18,13 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.WebUtils;
+
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -29,17 +40,17 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf().disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/user/welcome").permitAll()
+                    .requestMatchers("/user/welcome").permitAll()    // change this for welcome page or else
                 .and()
-                .formLogin()
-                .successHandler(mp_successHandler())
-                .failureHandler(mp_failureHandler())
-                .loginPage("/login").permitAll()
-//                .defaultSuccessUrl("/login/Success",true)
+                    .formLogin()
+                    .successHandler(mp_successHandler())
+                    .failureHandler(mp_failureHandler())
+                    .loginPage("/login").permitAll()
+    //                .defaultSuccessUrl("/login/Success",true)
                 .and()
-                .authorizeHttpRequests().requestMatchers("/**").permitAll()
+                    .authorizeHttpRequests().requestMatchers("/**").authenticated()
                 .and()
-                .build();
+                    .build();
     }
 
     @Bean
@@ -72,6 +83,35 @@ public class SecurityConfig {
         };
     }
 
+
+    private OncePerRequestFilter csrfHeaderFilter() {
+        return new OncePerRequestFilter() {
+            @Override
+            protected void doFilterInternal(HttpServletRequest request,
+                                            HttpServletResponse response, FilterChain filterChain)
+                    throws ServletException, IOException {
+                CsrfToken csrf = (CsrfToken) request.getAttribute(CsrfToken.class
+                        .getName());
+                if (csrf != null) {
+                    Cookie cookie = WebUtils.getCookie(request, "XSRF-TOKEN");
+                    String token = csrf.getToken();
+                    if (cookie == null || token != null
+                            && !token.equals(cookie.getValue())) {
+                        cookie = new Cookie("XSRF-TOKEN", token);
+                        cookie.setPath("/");
+                        response.addCookie(cookie);
+                    }
+                }
+                filterChain.doFilter(request, response);
+            }
+        };
+    }
+
+    private CsrfTokenRepository csrfTokenRepository() {
+        HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+        repository.setHeaderName("X-XSRF-TOKEN");
+        return repository;
+    }
 
 
 
--- academetrics/src/main/java/com/academetrics/academetrics/Controller/LoginController.java
+++ academetrics/src/main/java/com/academetrics/academetrics/Controller/LoginController.java
@@ -5,14 +5,41 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = "/login")
+@RequestMapping(value = "/")
 @CrossOrigin
 public class LoginController {
 
-    @GetMapping("/login")
-    public String loginUser(){return "Yee! you are logged in";}
+    // this is used for test security / login
+    @GetMapping("login")
+    public String loginUser(){return "<html \">\n" +
+            "  <head th:include=\"layout :: head(title=~{::title},links=~{})\">\n" +
+            "    <title>Please Login</title>\n" +
+            "  </head>\n" +
+            "  <body th:include=\"layout :: body\" th:with=\"content=~{::content}\">\n" +
+            "    <div th:fragment=\"content\">\n" +
+            "        <form name=\"f\" th:action=\"@{/login}\" method=\"post\">               \n" +
+            "            <fieldset>\n" +
+            "                <legend>Please Login</legend>\n" +
+            "                <div th:if=\"${param.error}\" class=\"alert alert-error\">    \n" +
+            "                    Invalid username and password.\n" +
+            "                </div>\n" +
+            "                <div th:if=\"${param.logout}\" class=\"alert alert-success\"> \n" +
+            "                    You have been logged out.\n" +
+            "                </div>\n" +
+            "                <label for=\"username\">Username</label>\n" +
+            "                <input type=\"text\" id=\"username\" name=\"username\"/>        \n" +
+            "                <label for=\"password\">Password</label>\n" +
+            "                <input type=\"password\" id=\"password\" name=\"password\"/>    \n" +
+            "                <div class=\"form-actions\">\n" +
+            "                    <button type=\"submit\" class=\"btn\">Log in</button>\n" +
+            "                </div>\n" +
+            "            </fieldset>\n" +
+            "        </form>\n" +
+            "    </div>\n" +
+            "  </body>\n" +
+            "</html>";}
 
-    @PostMapping("/login")
+    @PostMapping("login")
     public String checklogin(){
         return "loginCheck";
     }
